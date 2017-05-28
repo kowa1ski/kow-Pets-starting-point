127a988718d22121382edbfd20bb3a4ed2acf9b7
kowa1ski <kowalski@gmx.es>
2017-05-28 11:36:29 +0200

    Completado el update contemplando las posibles excepciones. Devuelve el número de filas que se han modificado

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -199,10 +199,40 @@ public class PetProvider extends ContentProvider {
      */
     private int updatePet (Uri uri, ContentValues values, String selection, String[] selectionArgs) {
 
-        // TODO: Update the selected pets in the pets database table with the given ContentValues
+        // If the COLUM_PET_NAME key is present, check that the name value is not null.
+        if (values.containsKey(PetContract.PetEntry.COLUMN_PET_NAME)) {
+            String name = values.getAsString(PetContract.PetEntry.COLUMN_PET_NAME);
+            if (name == null) {throw new IllegalArgumentException("Pet requires name");}
+        }
 
-        // TODO: Return the number of rows that were affected
-        return 0;
+        // If the COLUM_PET_GENDER key is present, check that the gender value is invalid.
+        if (values.containsKey(PetContract.PetEntry.COLUMN_PET_GENDER)) {
+            Integer gender = values.getAsInteger(PetContract.PetEntry.COLUMN_PET_GENDER);
+            if (gender == null || !PetContract.PetEntry.isValidGender(gender)) {
+                throw new IllegalArgumentException("Pet requires valid gender");
+            }
+        }
+
+        // If the COLUMN_PET_WEIGHT key is present, check that the weight value is valid.
+        if (values.containsKey(PetContract.PetEntry.COLUMN_PET_WEIGHT)) {
+            // Check that the weight is greater than or equal to 0 kg
+            Integer weight = values.getAsInteger(PetContract.PetEntry.COLUMN_PET_WEIGHT);
+            if (weight != null && weight < 0) { throw new IllegalArgumentException("Pet requires valid weight"); }
+        }
+
+        // No need to check the breed, any value is valid (including null)
+
+        //If there are no values to update, then don't try to update the database
+        if (values.size() == 0) { return 0; }
+
+
+
+        // Get writable database to update the data
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Returns the number of database rows affected by the update statement
+        return database.update(PetContract.PetEntry.TABLE_NAME, values, selection, selectionArgs);
+
     }
 
 }
