b1a99d97771676bfda4ddc37c1eeb4b2ddd2b807
kowa1ski <kowalski@gmx.es>
2017-05-31 07:41:11 +0200

    creada clase CursorAdapter. He implementado los métodos y el constructor con la bombilla roja :-) .

---
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -0,0 +1,27 @@
+package com.example.android.pets;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by Usuario on 31/05/2017.
+ */
+
+public class PetCursorAdapter extends CursorAdapter {
+    public PetCursorAdapter(Context context, Cursor c) {
+        super(context, c);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
