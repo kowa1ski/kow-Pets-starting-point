302cecc6e85048d3b777f7e04555c421bcf446a9
kowa1ski <kowalski@gmx.es>
2017-06-23 05:37:02 +0200

    Ahora ya pone que la raza es desconocida si es que el box se deja en blanco. antes salía en blanco también y ahora ya pone eso.

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -66,6 +67,12 @@ public class PetCursorAdapter extends CursorAdapter {
         // Read the pet attributes from the Cursor for the current pet
         String petName = cursor.getString(nameColumIndex);
         String petBreed = cursor.getString(breedColumIndex);
+
+        // If the pet breed is empty string or null, then use some default text
+        // that says "Unknown_breed", so the TextView isn't blank.
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = context.getString(R.string.unknown_breed);
+        }
 
         // Update the TextViews with the attribures for the current pet
         nameTextView.setText(petName);
